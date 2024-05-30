a291e31029c151b1cbb5dc557f212c6d3682f4c0
Icemap <qizhi.wang@pingcap.com>
2024-05-30 14:55:43 +0800

    feat: update setup information

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup
 
-setup(name='yahoo_fin',
-      version='0.8.9.3',
-      description="""Download historical stock prices (daily / weekly / monthly),
+setup(name='yahoo_fin_cheese',
+      version='0.8.9.5',
+      description="""Forked from yahoo_fin to update some outdated functions.
+                     Download historical stock prices (daily / weekly / monthly),
                      realtime-prices, fundamentals data, income statements, 
                      cash flows, analyst info, current cryptocurrency prices,
                      option chains, earnings history, and more with yahoo_fin.
                     """,
       url='http://theautomatic.net/yahoo_fin-documentation/',
-      author='Andrew Treadway',
-      author_email='opensourcecoder11@gmail.com',
+      author='Cheese Wong',
+      author_email='cheese.javaee@gmail.com',
       license='MIT',
       packages=['yahoo_fin'],
-      install_requires = ["requests_html", "feedparser", "requests", "pandas", "lxml_html_clean"],
+      install_requires = ["requests_html", "feedparser", "requests", "pandas", "pycryptodome"],
       keywords = ["yahoo finance", "stocks", "options", "fundamentals"],
 zip_safe=False)
