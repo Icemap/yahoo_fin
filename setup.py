f2dd468a7114b97fb712bcfd9acaa69e84c3ec57
Icemap <qizhi.wang@pingcap.com>
2024-05-30 16:08:27 +0800

    fix: repair the stock info outdated functions

--- setup.py
+++ setup.py
@@ -8,11 +8,19 @@ setup(name='yahoo_fin_cheese',
                      cash flows, analyst info, current cryptocurrency prices,
                      option chains, earnings history, and more with yahoo_fin.
                     """,
-      url='http://theautomatic.net/yahoo_fin-documentation/',
+      url='https://theautomatic.net/yahoo_fin-documentation/',
       author='Cheese Wong',
       author_email='cheese.javaee@gmail.com',
       license='MIT',
       packages=['yahoo_fin'],
-      install_requires = ["requests_html", "feedparser", "requests", "pandas", "pycryptodome"],
-      keywords = ["yahoo finance", "stocks", "options", "fundamentals"],
-zip_safe=False)
+      install_requires=[
+          "requests_html",
+          "feedparser",
+          "requests",
+          "pandas",
+          "pycryptodome",
+          "beautifulsoup4==4.11.1",
+          "bs4==0.0.2"
+      ],
+      keywords=["yahoo finance", "stocks", "options", "fundamentals"],
+      zip_safe=False)
